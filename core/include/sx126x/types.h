5aa76b6565a39e9ff0becfcddf3d64f9516c30c5
Travis Bubb <travisbubb14@gmail.com>
2025-10-06 20:53:42 -0400

    feat: add sx126x_status_t enumeration

---
+++ core/include/sx126x/types.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * @file sx126x.h
+ * @brief Public API for SX126x LoRa transceiver driver.
+ * @version 0.1
+ * @date 2025
+ */
+
+ #ifndef SX126X_TYPES_H
+ #define SX126X_TYPES_H
+
+/**
+ * @brief Status codes returned by the SX126X driver.
+ */
+typedef enum
+{
+  SX126X_OK = 0,
+  SX126X_ERR_INVALID_ARG,
+  SX126X_ERR_BUSY, /* driver or radio in a state that prevents request */
+  SX126X_ERR_TIMEOUT,
+  SX126X_ERR_NO_MEM,
+  SX126X_ERR_HAL,      /* underlying HAL reported an error */
+  SX126X_ERR_NOT_INIT, /* driver not initialized */
+  SX126X_ERR_UNKNOWN,
+} sx126x_status_t;
+
+ #endif // SX126X_TYPES_H
